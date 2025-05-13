e0a7f0731c8ee360c09f553a1955ad34c77d60fd
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-05-13 17:52:46 +0300

    ✅ feat(server): additional validation

--- server/src/schemas/simulation.py
+++ server/src/schemas/simulation.py
@@ -1,7 +1,9 @@
 import math
 from typing import List, Literal, Union
+from typing_extensions import Self
 
-from pydantic import BaseModel, Field
+from numpy import inner
+from pydantic import BaseModel, Field, model_validator
 
 from libs.shapes.core.enums import ShapeType
 
@@ -23,15 +25,27 @@ class SimulationBath(BaseModel):
 
 class SimulationRingShape(BaseModel):
     shape_type: Literal[ShapeType.RING] = ShapeType.RING
-    inner_radius: float = Field(gt=0, lt=10, examples=[5])
-    outer_radius: float = Field(gt=0, lt=20, examples=[10])
+    inner_radius: float = Field(gt=0, le=10, examples=[5])
+    outer_radius: float = Field(gt=0, le=20, examples=[10])
+
+    @model_validator(mode="after")
+    def size_validation(self) -> Self:
+        if self.inner_radius >= self.outer_radius:
+            raise ValueError("Inner raduis cannot be greater than outer radius")
+        return self
 
 
 class SimulationArrowShape(BaseModel):
     shape_type: Literal[ShapeType.ARROW] = ShapeType.ARROW
-    height: float = Field(gt=0, lt=10, examples=[6])
-    length: float = Field(gt=0, lt=10, examples=[8])
+    height: float = Field(gt=0, le=15, examples=[6])
+    length: float = Field(gt=0, le=15, examples=[8])
     angle: float = Field(ge=0, le=2 * math.pi, examples=[math.pi / 2])
+
+    @model_validator(mode="after")
+    def size_validation(self) -> Self:
+        if self.height > self.length:
+            raise ValueError("Height cannot be greater than length")
+        return self
 
 
 class SimulationConductor(BaseModel):
@@ -42,9 +56,15 @@ class SimulationConductor(BaseModel):
 
 
 class SimulationElectrode(BaseModel):
-    y_lower: float = Field(..., examples=[2], description="Lower Y bound. Units: [cm]")
-    y_upper: float = Field(examples=[16], description="Upper Y bound. Units: [cm]")
+    y_lower: float = Field(gt=0, examples=[2], description="Lower Y bound. Units: [cm]")
+    y_upper: float = Field(gt=0, examples=[16], description="Upper Y bound. Units: [cm]")
     potential: float = Field(gt=0, description="Constant electrode potential. Units: [V]")
+
+    @model_validator(mode="after")
+    def bounds_validation(self) -> Self:
+        if self.y_lower >= self.y_upper:
+            raise ValueError("Upper bound cannot be greater than lower bound")
+        return self
 
 
 class SimulationRequest(BaseModel):
@@ -53,6 +73,12 @@ class SimulationRequest(BaseModel):
     electrodes: SimulationElectrode = Field(
         description="Electrodes data. Located at left and right side of simulation area (x=0 and x=x_boundary)"
     )
+
+    @model_validator(mode="after")
+    def electrodes_validation(self) -> Self:
+        if self.electrodes.y_upper > self.bath.y_boundary:
+            raise ValueError("Electrode upper bound cannot be greater than bath upper bound")
+        return self
 
 
 class SimulationResponse(BaseModel):
