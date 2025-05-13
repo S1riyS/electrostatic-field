048b47c98cf2d5bdd1008e947c8ed087444d037e
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-05-13 19:05:43 +0300

    🔨 refactor(server): numpy to calculate gradient

--- server/src/libs/computations/gradient.py
+++ server/src/libs/computations/gradient.py
@@ -4,19 +4,5 @@ from numpy.typing import NDArray
 
 
 def gradient(f: NDArray[np.float64], dx: float, dy: float) -> NDArray[np.float64]:
-    # Инициализация массивов для производных
-    grad_x = np.zeros_like(f)
-    grad_y = np.zeros_like(f)
-
-    # Центральные разности для внутренних точек
-    grad_x[1:-1, :] = (f[2:, :] - f[:-2, :]) / (2 * dx)
-    grad_y[:, 1:-1] = (f[:, 2:] - f[:, :-2]) / (2 * dy)
-
-    # Граничные точки (односторонние разности)
-    grad_x[0, :] = (f[1, :] - f[0, :]) / dx
-    grad_x[-1, :] = (f[-1, :] - f[-2, :]) / dx
-
-    grad_y[:, 0] = (f[:, 1] - f[:, 0]) / dy
-    grad_y[:, -1] = (f[:, -1] - f[:, -2]) / dy
-
+    grad_x, grad_y = np.gradient(f, dx, dy)
     return np.dstack((grad_x, grad_y))
